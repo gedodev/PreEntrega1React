41d9f1d6a358a0b2790423ce853cea2c588959d2
gedodev <gdorta.wd@gmail.com>
2023-10-05 23:58:04 -0300

    PreEntrega1-Dorta

---
+++ e-commerce/src/components/items-list/ItemsList.jsx
@@ -0,0 +1,16 @@
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
+
+const ItemList = () => {
+    return(
+        <>
+        <Card>
+            <CardBody>
+                <Text>View a summary of all your customers over the last month.</Text>
+            </CardBody>
+        </Card>
+        </>
+    )
+}
+
+export default ItemList
